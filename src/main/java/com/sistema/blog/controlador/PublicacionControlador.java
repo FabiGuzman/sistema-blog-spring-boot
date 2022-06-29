f7837050624539aa3620410b595b0744ced744eb
FabiGuzman <fabi.guzman.gonzalez@gmail.com>
2022-06-29 10:47:28 -0500

    Second, commit 1 Hour, Minute 05 with 42 seconds I can update publications

--- src/main/java/com/sistema/blog/controlador/PublicacionControlador.java
+++ src/main/java/com/sistema/blog/controlador/PublicacionControlador.java
@@ -1,9 +1,14 @@
 package com.sistema.blog.controlador;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,4 +27,21 @@ public class PublicacionControlador {
 	public ResponseEntity<PublicacionDTO> guardarPublicacion(@RequestBody PublicacionDTO publicacionDTO) {
 		return new ResponseEntity<>(publicacionServicio.crearPublicacion(publicacionDTO),HttpStatus.CREATED);
 	}
+	
+	@GetMapping
+	public List<PublicacionDTO> listarPublicaciones() {
+		return publicacionServicio.obtenerTodasLasPublicaciones();
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<PublicacionDTO> obtenerPublicacionPorId(@PathVariable(name = "id") long id) {
+		return ResponseEntity.ok(publicacionServicio.obtenerPublicacionPorId(id));
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<PublicacionDTO> actualizarPublicacion(@RequestBody PublicacionDTO publicacionDTO, @PathVariable(name = "id") long id) {
+		PublicacionDTO publicacionRespuesta = publicacionServicio.actualizarPublicacion(publicacionDTO, id);
+		return new ResponseEntity<>(publicacionRespuesta,HttpStatus.OK);
+	}
+	
 }
--- src/main/java/com/sistema/blog/servicio/PublicacionServicio.java
+++ src/main/java/com/sistema/blog/servicio/PublicacionServicio.java
@@ -1,8 +1,13 @@
 package com.sistema.blog.servicio;
 
+import java.util.List;
+
 import com.sistema.blog.dto.PublicacionDTO;
 
 public interface PublicacionServicio {
 
 	public PublicacionDTO crearPublicacion(PublicacionDTO publicacion);
+	public List<PublicacionDTO> obtenerTodasLasPublicaciones();
+	public PublicacionDTO obtenerPublicacionPorId(long id);
+	public PublicacionDTO actualizarPublicacion(PublicacionDTO publicacionDTO,long id);
 }
